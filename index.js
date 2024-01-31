a5f9d39329e7cb5ba4bc7659ee6c9c0c59b48da0
mastero101 <mastershift_101@hotmail.com>
2024-01-31 14:11:17 -0600

    change port

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.delete('/:id', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORTSERVICE || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
