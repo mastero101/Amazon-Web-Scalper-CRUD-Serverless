76a05e6b2deabd8df5050199e3623a037e2a84dd
mastero101 <mastershift_101@hotmail.com>
2024-01-31 14:08:40 -0600

    rollback for use planetscale

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2/promise'); // Usar mysql2/promise para soporte de promesas
-const fs = require('fs'); // Importar el módulo 'fs' para leer el certificado CA
+const mysql = require('mysql');
 
 require('dotenv').config();
 
 const dbConfig = {
     host: process.env.HOST,
-    port: process.env.PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    ssl: {
-        ca: fs.readFileSync('./ca.pem') // Ruta al certificado CA
-    }
+    port: 3306,
+    ssl: true
 };
 
-const pool = mysql.createPool(dbConfig); // No es necesario usar .promise() si ya estás usando mysql2/promise
+const pool = mysql.createPool(dbConfig);
 
 const app = express();
 
@@ -24,10 +21,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 app.all('/', (req, res) => {
@@ -35,89 +32,96 @@ app.all('/', (req, res) => {
     res.send('Webscalper Amazon Works');
 });
 
-app.get('/items', async (req, res) => {
-    try {
-        const [rows] = await pool.execute('SELECT * FROM amazon');
-        res.send(rows);
-    } catch (error) {
-        console.error('Error al obtener datos de la base de datos:', error);
-        res.status(500).send('Error al obtener datos de la base de datos.');
-    }
+app.get('/items', (req, res) => {
+    pool.query('SELECT * FROM amazon', (error, results, fields) => {
+        if (error) {
+            console.error('Error al obtener datos de la base de datos:', error);
+            res.status(500).send('Error al obtener datos de la base de datos.');
+            return;
+        }
+        res.send(results);
+    });
 });
 
-app.get('/items/prices', async (req, res) => {
-    try {
-        const [rows] = await pool.execute("SELECT id, precio FROM amazon WHERE precio");
-        res.send(rows);
-    } catch (error) {
-        console.error('Error al obtener datos de la base de datos:', error);
-        res.status(500).send('Error al obtener datos de la base de datos.');
-    }
+app.get('/items/prices', (req, res) => {
+    pool.query("SELECT id, precio FROM amazon WHERE precio", (error, results, fields) => {
+        if (error) {
+            console.error('Error al obtener datos de la base de datos:', error);
+            res.status(500).send('Error al obtener datos de la base de datos.');
+            return;
+        }
+        res.send(results);
+    });
 });
 
-app.get('/models/:model', async (req, res) => {
+app.get('/models/:model', (req, res) => {
     const model = req.params.model;
     const query = 'SELECT * FROM amazon WHERE modelo LIKE ?';
     const searchTerm = `%${model}%`;
-    try {
-        const [rows] = await pool.execute(query, searchTerm);
-        if (rows.length === 0) {
-            res.status(404).send('No se encontró ningún usuario con el ID proporcionado.');
-        } else {
-            res.send(rows[0]);
+    pool.query(query, searchTerm, (error, results, fields) => {
+      if (error) {
+        console.error('Error al obtener datos de la base de datos:', error);
+        res.status(500).send('Error al obtener datos de la base de datos.');
+        return;
+      }
+      if (results.length === 0) {
+        res.status(404).send('No se encontró ningún usuario con el ID proporcionado.');
+        return;
+      }
+      res.send(results[0]);
+    });
+});
+
+app.get('/procesadores', (req, res) => {
+    pool.query("SELECT modelo, precio, tienda, url, img, consumo, socket FROM componentes WHERE tipo = 'procesador'", (error, results, fields) => {
+        if (error) {
+            console.error('Error al obtener datos de la base de datos:', error);
+            res.status(500).send('Error al obtener datos de la base de datos.');
+            return;
         }
-    } catch (error) {
-        console.error('Error al obtener datos de la base de datos:', error);
-        res.status(500).send('Error al obtener datos de la base de datos.');
-    }
+        res.send(results);
+    });
 });
 
-app.get('/procesadores', async (req, res) => {
-    try {
-        const [rows] = await pool.execute("SELECT modelo, precio, tienda, url, img, consumo, socket FROM componentes WHERE tipo = 'procesador'");
-        res.send(rows);
-    } catch (error) {
-        console.error('Error al obtener datos de la base de datos:', error);
-        res.status(500).send('Error al obtener datos de la base de datos.');
-    }
-});
-
-app.post('/item', async (req, res) => {
+app.post('/item', (req, res) => {
     const data = req.body;
-    try {
-        const result = await pool.query('INSERT INTO amazon SET ?', data);
-        console.log("Item Registrado");
-        res.send(result);
-    } catch (error) {
-        console.error('Error al insertar datos en la base de datos:', error);
-        res.status(500).send('Error al insertar datos en la base de datos.');
-    }
+    pool.query('INSERT INTO amazon SET ?', data, (error, results, fields) => {
+        if (error) {
+            console.error('Error al insertar datos en la base de datos:', error);
+            res.status(500).send('Error al insertar datos en la base de datos.');
+            return;
+        }
+	console.log("Item Registrado");
+        res.send(results);
+    });
 });
 
-app.put('/:id', async (req, res) => {
+app.put('/:id', (req, res) => {
     const id = req.params.id;
     const data = req.body;
-    try {
-        const result = await pool.query('UPDATE amazon SET ? WHERE id = ?', [data, id]);
-        res.send(result);
-    } catch (error) {
-        console.error('Error al actualizar datos en la base de datos:', error);
-        res.status(500).send('Error al actualizar datos en la base de datos.');
-    }
+    pool.query('UPDATE amazon SET ? WHERE id = ?', [data, id], (error, results, fields) => {
+        if (error) {
+            console.error('Error al actualizar datos en la base de datos:', error);
+            res.status(500).send('Error al actualizar datos en la base de datos.');
+            return;
+        }
+        res.send(results);
+    });
 });
 
-app.delete('/:id', async (req, res) => {
+app.delete('/:id', (req, res) => {
     const id = req.params.id;
-    try {
-        const result = await pool.query('DELETE FROM amazon WHERE id = ?', id);
-        res.send(result);
-    } catch (error) {
-        console.error('Error al eliminar datos de la base de datos:', error);
-        res.status(500).send('Error al eliminar datos de la base de datos.');
-    }
+    pool.query('DELETE FROM amazon WHERE id = ?', id, (error, results, fields) => {
+        if (error) {
+            console.error('Error al eliminar datos de la base de datos:', error);
+            res.status(500).send('Error al eliminar datos de la base de datos.');
+            return;
+        }
+        res.send(results);
+    });
 });
 
-const PORT = process.env.PORTSERVICE || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
